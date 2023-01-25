4eb0473959db245c72f72017dba2e7dc0ec92a8f
kekexin714 <kekexin714@gmail.com>
2023-01-25 19:08:33 +0000

    program to calculate mean projection from a 3d volume

--- src/programs/calculate_average_template/calculate_average_template.cpp
+++ src/programs/calculate_average_template/calculate_average_template.cpp
@@ -38,15 +38,17 @@ void CalculateAverageTemplateApp::DoInteractiveUserInput( ) {
     float    angular_step;
     float    in_plane_angular_step;
     float    defocus_1, defocus_2;
+    wxString input_search_images;
 
     int max_threads;
 
     UserInput* my_input = new UserInput("CalculateAverageTemplate", 1.0);
 
     input_reconstruction  = my_input->GetFilenameFromUser("Input reconstruction", "The 3D reconstruction from which projections are calculated", "my_reconstruction.mrc", true);
+    input_search_images   = my_input->GetFilenameFromUser("Input images to be searched", "The input image stack, containing the images that should be searched", "image_stack.mrc", true);
     angular_step          = my_input->GetFloatFromUser("Out of plane angular step (0.0 = set automatically)", "Angular step size for global grid search", "0.0", 0.0);
     in_plane_angular_step = my_input->GetFloatFromUser("In plane angular step (0.0 = set automatically)", "Angular step size for in-plane rotations during the search", "0.0", 0.0);
-    apply_CTF             = true; // todo add this option
+    apply_CTF             = my_input->GetYesNoFromUser("Apply ctf?", "apply ctf to image?", "Yes");
     apply_shifts          = false;
     write_std_image       = my_input->GetYesNoFromUser("Write std image?", "Do you need to output std image of template?", "Yes");
     ouput_average_image   = my_input->GetFilenameFromUser("Output average image", "The output average image, containing the 2D projection", "my_projection_mean.mrc", false);
@@ -79,7 +81,7 @@ void CalculateAverageTemplateApp::DoInteractiveUserInput( ) {
     delete my_input;
 
     //	my_current_job.Reset(14);
-    my_current_job.ManualSetArguments("ttbtfffftbbbbffiff",
+    my_current_job.ManualSetArguments("ttbtfffftbbbbffifft",
                                       input_reconstruction.ToUTF8( ).data( ),
                                       ouput_average_image.ToUTF8( ).data( ),
                                       write_std_image,
@@ -97,30 +99,32 @@ void CalculateAverageTemplateApp::DoInteractiveUserInput( ) {
                                       in_plane_angular_step,
                                       max_threads,
                                       defocus_1,
-                                      defocus_2);
+                                      defocus_2,
+                                      input_search_images);
 }
 
 // override the do calculation method which will be what is actually run..
 
 bool CalculateAverageTemplateApp::DoCalculation( ) {
-    wxString input_reconstruction  = my_current_job.arguments[0].ReturnStringArgument( );
-    wxString ouput_average_image   = my_current_job.arguments[1].ReturnStringArgument( );
-    bool     write_std_image       = my_current_job.arguments[2].ReturnBoolArgument( );
-    wxString ouput_std_image       = my_current_job.arguments[3].ReturnStringArgument( );
-    float    pixel_size            = my_current_job.arguments[4].ReturnFloatArgument( );
-    float    mask_radius           = my_current_job.arguments[5].ReturnFloatArgument( );
-    float    wanted_SNR            = my_current_job.arguments[6].ReturnFloatArgument( );
-    float    padding               = my_current_job.arguments[7].ReturnFloatArgument( );
-    wxString my_symmetry           = my_current_job.arguments[8].ReturnStringArgument( );
-    bool     apply_CTF             = my_current_job.arguments[9].ReturnBoolArgument( );
-    bool     apply_shifts          = my_current_job.arguments[10].ReturnBoolArgument( );
-    bool     apply_mask            = my_current_job.arguments[11].ReturnBoolArgument( );
-    bool     add_noise             = my_current_job.arguments[12].ReturnBoolArgument( );
-    float    angular_step          = my_current_job.arguments[13].ReturnFloatArgument( );
-    float    in_plane_angular_step = my_current_job.arguments[14].ReturnFloatArgument( );
-    int      max_threads           = my_current_job.arguments[15].ReturnIntegerArgument( );
-    float    defocus_1             = my_current_job.arguments[16].ReturnFloatArgument( );
-    float    defocus_2             = my_current_job.arguments[17].ReturnFloatArgument( );
+    wxString input_reconstruction         = my_current_job.arguments[0].ReturnStringArgument( );
+    wxString ouput_average_image          = my_current_job.arguments[1].ReturnStringArgument( );
+    bool     write_std_image              = my_current_job.arguments[2].ReturnBoolArgument( );
+    wxString ouput_std_image              = my_current_job.arguments[3].ReturnStringArgument( );
+    float    pixel_size                   = my_current_job.arguments[4].ReturnFloatArgument( );
+    float    mask_radius                  = my_current_job.arguments[5].ReturnFloatArgument( );
+    float    wanted_SNR                   = my_current_job.arguments[6].ReturnFloatArgument( );
+    float    padding                      = my_current_job.arguments[7].ReturnFloatArgument( );
+    wxString my_symmetry                  = my_current_job.arguments[8].ReturnStringArgument( );
+    bool     apply_CTF                    = my_current_job.arguments[9].ReturnBoolArgument( );
+    bool     apply_shifts                 = my_current_job.arguments[10].ReturnBoolArgument( );
+    bool     apply_mask                   = my_current_job.arguments[11].ReturnBoolArgument( );
+    bool     add_noise                    = my_current_job.arguments[12].ReturnBoolArgument( );
+    float    angular_step                 = my_current_job.arguments[13].ReturnFloatArgument( );
+    float    in_plane_angular_step        = my_current_job.arguments[14].ReturnFloatArgument( );
+    int      max_threads                  = my_current_job.arguments[15].ReturnIntegerArgument( );
+    float    defocus_1                    = my_current_job.arguments[16].ReturnFloatArgument( );
+    float    defocus_2                    = my_current_job.arguments[17].ReturnFloatArgument( );
+    wxString input_search_images_filename = my_current_job.arguments[18].ReturnStringArgument( );
 
     Image               projection_image;
     Image               final_image;
@@ -129,6 +133,27 @@ bool CalculateAverageTemplateApp::DoCalculation( ) {
     Image               sum_of_square_image;
     ReconstructedVolume input_3d;
     Image               projection_3d;
+    ImageFile           input_search_image_file;
+    Image               input_image;
+    Image               projection_filter;
+
+    // input_search_image_file.OpenFile(input_search_images_filename.ToStdString( ), false);
+    input_search_image_file.OpenFile("mature60S_DF5000A_z250nm_1.06Apix.mrc", false);
+    input_image.ReadSlice(&input_search_image_file, 1);
+    input_image.ReplaceOutliersWithMean(5.0f);
+    input_image.ForwardFFT( );
+    input_image.SwapRealSpaceQuadrants( );
+
+    Curve whitening_filter;
+    Curve number_of_terms;
+    whitening_filter.SetupXAxis(0.0, 0.5 * sqrtf(2.0), int((input_image.logical_x_dimension / 2.0 + 1.0) * sqrtf(2.0) + 1.0));
+    number_of_terms.SetupXAxis(0.0, 0.5 * sqrtf(2.0), int((input_image.logical_x_dimension / 2.0 + 1.0) * sqrtf(2.0) + 1.0));
+
+    input_image.ZeroCentralPixel( ); // equivalent to subtracting mean in real space
+    input_image.Compute1DPowerSpectrumCurve(&whitening_filter, &number_of_terms);
+    whitening_filter.SquareRoot( );
+    whitening_filter.Reciprocal( );
+    whitening_filter.MultiplyByConstant(1.0f / whitening_filter.ReturnMaximumValue( ));
 
     int image_counter                      = 0;
     int number_of_projections_to_calculate = 0;
@@ -182,7 +207,7 @@ bool CalculateAverageTemplateApp::DoCalculation( ) {
         }
     }
 
-    global_euler_search.CalculateGridSearchPositions(false); // todo: what whitening filter? -> can be applied in compare_template
+    global_euler_search.CalculateGridSearchPositions(false);
 
     int first_search_position = 0;
     int last_search_position  = global_euler_search.number_of_search_positions - 1;
@@ -191,16 +216,14 @@ bool CalculateAverageTemplateApp::DoCalculation( ) {
     float current_psi;
     // TODO unroll these loops and multiply the product.
 
-    int number_of_rotations = 0;
-    for ( current_psi = psi_start; current_psi <= psi_max; current_psi += psi_step ) {
-        number_of_rotations++;
-    }
-
     MRCFile input_file(input_reconstruction.ToStdString( ), false);
     MRCFile output_avg_file(ouput_average_image.ToStdString( ), true);
 
     AnglesAndShifts my_parameters;
     CTF             my_ctf;
+
+    my_ctf.Init(microscope_voltage_kv, microscope_spherical_aberration_mm, amplitude_contrast, defocus_1, defocus_2, defocus_angle, 0.0, 0.0, 0.0, pixel_size, deg_2_rad(phase_shift));
+    my_ctf.SetDefocus(defocus_1 / pixel_size, defocus_2 / pixel_size, deg_2_rad(defocus_angle));
 
     if ( (input_file.ReturnXSize( ) != input_file.ReturnYSize( )) || (input_file.ReturnXSize( ) != input_file.ReturnZSize( )) ) {
         MyPrintWithDetails("Error: Input reconstruction is not cubic\n");
@@ -223,6 +246,11 @@ bool CalculateAverageTemplateApp::DoCalculation( ) {
             number_of_projections_to_calculate++;
         }
     }
+
+    int number_of_rotations = 0;
+    for ( current_psi = psi_start; current_psi <= psi_max; current_psi += psi_step ) {
+        number_of_rotations++;
+    }
     ProgressBar* my_progress = new ProgressBar(number_of_projections_to_calculate);
     projection_3d.CopyFrom(input_3d.density_map);
     projection_image.Allocate(input_file.ReturnXSize( ), input_file.ReturnYSize( ), false);
@@ -232,21 +260,26 @@ bool CalculateAverageTemplateApp::DoCalculation( ) {
     sum_image.SetToConstant(0.0f);
     sum_of_square_image.Allocate(input_file.ReturnXSize( ), input_file.ReturnYSize( ), true);
     sum_of_square_image.SetToConstant(0.0f);
+    projection_filter.Allocate(input_file.ReturnXSize( ), input_file.ReturnYSize( ), true);
+    projection_filter.SetToConstant(0.0f);
+    projection_filter.CalculateCTFImage(my_ctf);
+    projection_filter.ApplyCurveFilter(&whitening_filter);
 
-#pragma omp parallel for num_threads(max_threads) default(shared) private(current_angular_position, current_psi, input_parameters, my_parameters, my_ctf, variance) firstprivate(projection_image, final_image, final_image_squared)
+#pragma omp parallel for num_threads(max_threads) default(shared) private(current_angular_position, current_psi, input_parameters, my_parameters, variance) firstprivate(projection_image, final_image, final_image_squared)
     for ( current_angular_position = first_search_position; current_angular_position <= last_search_position; current_angular_position++ ) {
         RandomNumberGenerator local_random_generator(int(fabsf(global_random_number_generator.GetUniformRandom( ) * 50000)), true); // todo
         for ( current_psi = psi_start; current_psi <= psi_max; current_psi += psi_step ) {
             int view_counter = (current_angular_position - first_search_position) * int((psi_max - psi_start) / psi_step + 1) + int((current_psi - psi_start) / psi_step);
-            wxPrintf("view_counter = %i\n", view_counter);
+
+            wxPrintf("phi theta psi = %f %f %f\n", global_euler_search.list_of_search_parameters[current_angular_position][0], global_euler_search.list_of_search_parameters[current_angular_position][1], current_psi);
             my_parameters.Init(global_euler_search.list_of_search_parameters[current_angular_position][0], global_euler_search.list_of_search_parameters[current_angular_position][1], current_psi, 0.0, 0.0f);
-            my_ctf.Init(microscope_voltage_kv, microscope_spherical_aberration_mm, amplitude_contrast, defocus_1, defocus_2, defocus_angle, 0.0, 0.0, 0.0, pixel_size, phase_shift, -beam_tilt_x / 1000.0f, -beam_tilt_y / 1000.0f, -image_shift_x, -image_shift_y);
+            // my_ctf.Init(microscope_voltage_kv, microscope_spherical_aberration_mm, amplitude_contrast, defocus_1, defocus_2, defocus_angle, 0.0, 0.0, 0.0, pixel_size, phase_shift, -beam_tilt_x / 1000.0f, -beam_tilt_y / 1000.0f, -image_shift_x, -image_shift_y);
             projection_3d.ExtractSlice(projection_image, my_parameters);
-            projection_image.complex_values[0] = projection_3d.complex_values[0];
-            if ( apply_CTF )
-                projection_image.ApplyCTF(my_ctf, false, true);
 
             projection_image.SwapRealSpaceQuadrants( );
+            if ( apply_CTF )
+                projection_image.MultiplyPixelWise(projection_filter);
+            projection_image.complex_values[0] = projection_3d.complex_values[0];
             projection_image.BackwardFFT( );
 
             final_image.CopyFrom(&projection_image);
