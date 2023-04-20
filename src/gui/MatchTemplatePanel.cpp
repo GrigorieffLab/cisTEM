742f16af7e5daf404eef2a76e8069d7e2e42fa2e
Johannes Elferich <jojotux123@hotmail.com>
2023-04-20 18:26:01 +0000

    Test one gpu per image

--- src/gui/MatchTemplatePanel.cpp
+++ src/gui/MatchTemplatePanel.cpp
@@ -673,7 +673,7 @@ void MatchTemplatePanel::StartEstimationClick(wxCommandEvent& event) {
 
     if ( use_gpu ) {
         //	number_of_jobs_per_image_in_gui = std::max((int)1,number_of_processes / 2); // Using two threads in each job
-        number_of_jobs_per_image_in_gui = number_of_processes; // Using two threads in each job
+        number_of_jobs_per_image_in_gui = 1; // Using two threads in each job
 
         number_of_jobs = number_of_jobs_per_image_in_gui * active_group.number_of_members;
 
