e4e022c7e52f235e6184b2701a8b5ce0a9d0ad3f
kekexin714 <kekexin714@gmail.com>
2024-04-30 14:08:20 -0400

    calculate 2DTM p-value using SNR and z-score; add Eigen library for numeric calculations

--- src/programs/make_template_result_dev/make_template_result_dev.cpp
+++ src/programs/make_template_result_dev/make_template_result_dev.cpp
@@ -1,11 +1,145 @@
 #include "../../core/core_headers.h"
 
 #include "../../constants/constants.h"
-#include <vector> // for std::vector
-#include <algorithm> // for std::max
-#include <utility> // for std::pair
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <numeric>
+#include "../../include/Eigen/Dense"
 
 using namespace std;
+using namespace Eigen;
+
+vector<float> CalculateProbit(const vector<float>& x) {
+
+    // Step 1: Create a vector of pairs (value, original index)
+    vector<pair<float, int>> indexedValues;
+    for ( int i = 0; i < x.size( ); ++i ) {
+        indexedValues.push_back(make_pair(x[i], i));
+    }
+
+    // Step 2: Sort the vector of pairs by the values
+    sort(indexedValues.begin( ), indexedValues.end( ), [](const pair<float, int>& a, const pair<float, int>& b) {
+        return a.first < b.first; // sort in ascending order
+    });
+
+    // Step 3: Create a vector to store the ranks
+    vector<int> ranks(x.size( ));
+
+    // Assign ranks based on sorted positions
+    for ( int i = 0; i < indexedValues.size( ); ++i ) {
+        ranks[indexedValues[i].second] = i + 1; // rank starts from 1
+    }
+
+    vector<float> probit_x;
+    for ( int rank : ranks ) {
+        float rank_tmp = (rank - 0.5) / x.size( );
+        float probit   = sqrt(2.0) * cisTEM_erfinv(2 * rank_tmp - 1.0);
+        probit_x.push_back(probit);
+    }
+
+    return probit_x;
+}
+
+float rho_g2(float x, float y, const Matrix2f& C) {
+    float detC        = C.determinant( ); // Determinant of the matrix C
+    float coefficient = 1 / (2 * M_PI) * sqrt(detC);
+    float exponent    = -0.5 * (x * x * C(0, 0) + x * y * (C(0, 1) + C(1, 0)) + y * y * C(1, 1));
+    return coefficient * exp(exponent);
+}
+
+vector<float> linspace(float start, float end, int num) {
+    vector<float> linspaced;
+    float         delta = (end - start) / (num - 1);
+    for ( int i = 0; i < num; ++i ) {
+        linspaced.push_back(start + delta * i);
+    }
+    return linspaced;
+}
+
+pair<MatrixXf, MatrixXf> CalculateAnisotropicGaussianForProbit(const vector<float>& pro_x1_, const vector<float>& pro_x2_) {
+    float         pro_lim[2] = {-4.5, 4.5};
+    int           n_pro_x    = 128;
+    int           n_pro_y    = n_pro_x + 1;
+    vector<float> pro_x_     = linspace(pro_lim[0], pro_lim[1], n_pro_x);
+    vector<float> pro_y_     = linspace(pro_lim[0], pro_lim[1], n_pro_y);
+
+    float pro_dx = (pro_x_.back( ) - pro_x_.front( )) / (pro_x_.size( ) - 1);
+    float pro_dy = (pro_y_.back( ) - pro_y_.front( )) / (pro_y_.size( ) - 1);
+
+    // mesh grid
+    MatrixXf pro_x__(n_pro_y, n_pro_x);
+    MatrixXf pro_y__(n_pro_y, n_pro_x);
+    for ( int i = 0; i < n_pro_y; ++i ) {
+        for ( int j = 0; j < n_pro_x; ++j ) {
+            pro_x__(i, j) = pro_x_[j];
+            pro_y__(i, j) = pro_y_[i];
+        }
+    }
+
+    // [x1,x2]
+    MatrixXf tmp(2, pro_x1_.size( ));
+    for ( size_t i = 0; i < pro_x1_.size( ); ++i ) {
+        tmp(0, i) = pro_x1_[i];
+        tmp(1, i) = pro_x2_[i];
+    }
+
+    // calculate covariance matrix
+    MatrixXf              Cinv__ = tmp * tmp.transpose( ) / pro_x1_.size( );
+    EigenSolver<MatrixXf> es(Cinv__);
+    MatrixXf              Dinv_  = es.eigenvalues( ).real( );
+    MatrixXf              Uinv__ = es.eigenvectors( ).real( );
+
+    return {Dinv_, Uinv__};
+}
+
+vector<float> Calculate1QPValue(const vector<float>& ag_x1_,
+                                const vector<float>& ag_x2_,
+                                const Vector2f&      Dinv_,
+                                const Matrix2f&      Uinv__) {
+    int           n_r = ag_x1_.size( );
+    vector<float> p1q_equ_r_(n_r, 0.0);
+    float         tmp_a = sqrt(Dinv_.maxCoeff( ));
+    float         tmp_b = sqrt(Dinv_.minCoeff( ));
+
+    Eigen::Matrix2f Uinv = Uinv__;
+    if ( Dinv_[0] <= Dinv_[1] ) {
+        Uinv.col(0) = Uinv__.col(1);
+        Uinv.col(1) = Uinv__.col(0);
+    }
+    if ( Uinv(1, 0) < 0 && Uinv(0, 0) < 0 ) {
+        Uinv(1, 0) *= -1;
+        Uinv(0, 0) *= -1;
+    }
+
+    float tmp_w = atan2(Uinv(1, 0), Uinv(0, 0));
+    // wxPrintf("Major axis = %f\n", tmp_w * 180 / M_PI);
+
+    float tmp_gamma = atan2(1.0, 0.5 * sin(2 * tmp_w) * (tmp_b / tmp_a - tmp_a / tmp_b));
+
+    for ( int i = 0; i < n_r; ++i ) {
+        float ag_x1   = ag_x1_[i];
+        float ag_x2   = ag_x2_[i];
+        float p1q_equ = 1.0;
+        if ( ag_x1 > 0.5 && ag_x2 > 0.5 ) {
+            float tmp_x_0 = cos(tmp_w) * ag_x1 + sin(tmp_w) * ag_x2;
+            float tmp_x_1 = -sin(tmp_w) * ag_x1 + cos(tmp_w) * ag_x2;
+            float tmp_y_0 = tmp_x_0 / max(1e-12f, tmp_a);
+            float tmp_y_1 = tmp_x_1 / max(1e-12f, tmp_b);
+            float tmp_y_r = sqrt(tmp_y_0 * tmp_y_0 + tmp_y_1 * tmp_y_1);
+            p1q_equ       = exp(-tmp_y_r * tmp_y_r / 2) * tmp_gamma / (2 * M_PI);
+        }
+        p1q_equ_r_[i] = p1q_equ;
+    }
+
+    vector<float> negative_log_p1q_equ_r_(n_r);
+    for ( int i = 0; i < n_r; ++i ) {
+        negative_log_p1q_equ_r_[i] = -log(p1q_equ_r_[i]);
+    }
+
+    return negative_log_p1q_equ_r_;
+}
 
 class
         MakeTemplateResultDev : public MyApp {
@@ -48,7 +182,6 @@ void MakeTemplateResultDev::DoInteractiveUserInput( ) {
     min_peak_radius                 = my_input->GetIntFromUser("Min Peak Radius (px.)", "Essentially the minimum closeness for peaks", "10", 1);
     pixel_size                      = my_input->GetFloatFromUser("Pixel size of images (A)", "Pixel size of input images in Angstroms", "1.0", 0.0);
     ignore_N_pixels_from_the_border = my_input->GetIntFromUser("Ignore N pixels from the edge of the MIP", "Defaults to 1/2 the template dimension (-1)", "-1", -1);
-
     delete my_input;
 
     //	my_current_job.Reset(14);
@@ -104,12 +237,12 @@ bool MakeTemplateResultDev::DoCalculation( ) {
     long  address;
     long  text_file_access_type;
 
-    float                          coordinates[8];
-    vector<tuple<float, int, int>> localMaxima; // Peak, Row, Column
+    vector<float>                         coordinates;
+    vector<tuple<float, int, int, float>> localMaxima; // z-score, Row, Column, SNR
 
     text_file_access_type = OPEN_TO_WRITE;
-    NumericTextFile coordinate_file(xyz_coords_filename, text_file_access_type, 8);
-    coordinate_file.WriteCommentLine("         Psi          Theta            Phi              X              Y              Z      PixelSize           Peak");
+    NumericTextFile coordinate_file(xyz_coords_filename, text_file_access_type, 10);
+    coordinate_file.WriteCommentLine("         Psi          Theta            Phi              X              Y              Z      PixelSize           z-score           SNR           pval");
 
     mip_image.QuickAndDirtyReadSlice(input_mip_filename.ToStdString( ), result_number);
     scaled_mip_image.QuickAndDirtyReadSlice(input_scaled_mip_filename.ToStdString( ), result_number);
@@ -129,7 +262,6 @@ bool MakeTemplateResultDev::DoCalculation( ) {
 
     wxPrintf("\n");
 
-    // int pixel_counter = 0;
     for ( int i = ignore_N_pixels_from_the_border; i <= mip_x_dimension - ignore_N_pixels_from_the_border; i++ ) {
         for ( int j = ignore_N_pixels_from_the_border; j <= mip_y_dimension - ignore_N_pixels_from_the_border; j++ ) {
 
@@ -145,7 +277,7 @@ bool MakeTemplateResultDev::DoCalculation( ) {
                     if ( ni >= 0 && ni < mip_x_dimension && nj >= 0 && nj < mip_y_dimension ) {
 
                         if ( scaled_mip_image.ReturnRealPixelFromPhysicalCoord(ni, nj, 0) > maxVal ) {
-                            maxVal     = scaled_mip_image.ReturnRealPixelFromPhysicalCoord(ni, nj, 0); // CHECKME: should ni,nj be logical or physical coordinates?
+                            maxVal     = scaled_mip_image.ReturnRealPixelFromPhysicalCoord(ni, nj, 0);
                             isLocalMax = false;
                         }
                     }
@@ -153,36 +285,54 @@ bool MakeTemplateResultDev::DoCalculation( ) {
             }
 
             if ( isLocalMax && maxVal == scaled_mip_image.ReturnRealPixelFromPhysicalCoord(i, j, 0) ) {
-                localMaxima.emplace_back(maxVal, i, j);
-                // write into output coordinates
-
+                localMaxima.emplace_back(maxVal, i, j, mip_image.ReturnRealPixelFromPhysicalCoord(i, j, 0));
                 number_of_peaks_found++;
             }
-
-            //pixel_counter++;
         }
-        //pixel_counter += scaled_mip_image.padding_jump_value;
     }
 
-    // Sort based on the local maxima values (descending order)
-    sort(localMaxima.begin( ), localMaxima.end( ), [](const tuple<float, int, int>& a, const tuple<float, int, int>& b) {
-        return get<0>(a) > get<0>(b); // Sorting by first element of tuple, which is the max value
+    // Sort based on the local z-score maxima (descending order)
+    sort(localMaxima.begin( ), localMaxima.end( ), [](const tuple<float, int, int, float>& a, const tuple<float, int, int, float>& b) {
+        return get<0>(a) > get<0>(b); // Sorting by first element of tuple, which is the z-score
     });
 
-    int coord_x, coord_y;
-    for ( const auto& entry : localMaxima ) {
-        coord_x = get<1>(entry);
-        coord_y = get<2>(entry);
+    // calculate 2DTM p-value
+    vector<float> zScores;
+    vector<float> SNRs;
 
-        coordinates[0] = psi_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0);
-        coordinates[1] = theta_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0);
-        coordinates[2] = phi_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0);
-        coordinates[3] = coord_x * pixel_size;
-        coordinates[4] = coord_y * pixel_size;
-        coordinates[5] = defocus_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0);
-        coordinates[6] = pixel_size_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0);
-        coordinates[7] = get<0>(entry);
-        coordinate_file.WriteLine(coordinates);
+    // extract z-scores as vector
+    for ( int i = 0; i < localMaxima.size( ); ++i ) {
+        zScores.push_back(get<0>(localMaxima[i]));
+        SNRs.push_back(get<3>(localMaxima[i]));
+    }
+
+    // calculate quantile transformed values (probit function)
+    auto pro_zscores = CalculateProbit(zScores);
+    auto pro_snrs    = CalculateProbit(SNRs);
+    // estimate joint anisotropic gaussian from quantile transformed data
+    auto [Dinv_, Uinv__] = CalculateAnisotropicGaussianForProbit(pro_zscores, pro_snrs);
+
+    auto neg_log_p1q_ = Calculate1QPValue(pro_zscores, pro_snrs, Dinv_, Uinv__);
+
+    int coord_x, coord_y;
+    for ( int rowId = 0; rowId < localMaxima.size( ); ++rowId ) {
+        coordinates.clear( ); // efficient for reusing vector
+        auto& current_peak = localMaxima[rowId];
+        coord_x            = get<1>(current_peak);
+        coord_y            = get<2>(current_peak);
+        coordinates.push_back(psi_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0));
+        coordinates.push_back(theta_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0));
+        coordinates.push_back(phi_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0));
+        coordinates.push_back(coord_x * pixel_size);
+        coordinates.push_back(coord_y * pixel_size);
+        coordinates.push_back(defocus_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0));
+        coordinates.push_back(pixel_size_image.ReturnRealPixelFromPhysicalCoord(coord_x, coord_y, 0));
+        coordinates.push_back(get<0>(current_peak)); // z-score
+        coordinates.push_back(get<3>(current_peak)); // SNR
+        coordinates.push_back(neg_log_p1q_[rowId]); // neg log p-value
+
+        // optional coordinate_file length
+        coordinate_file.WriteLine(coordinates.data( ));
     }
 
     if ( is_running_locally == true ) {
